129d8a6d1d2106768ae918b78dbb5ed8fb5f7dad
sjarso <stephenjacob815@gmail.com>
2026-02-19 15:56:51 +0300

    add delete function to task manager

--- main.go
+++ main.go
@@ -7,15 +7,20 @@ import (
 	//"strings"
 	//"strconv"
 )
+var tasks []Task
+var taskIndex map[int]*Task
 
+type TaskManager struct{
+	tasks []Task
+	taskIndex map[int]*Task
+}
 //struct - groups related data
 type Task struct {
 	Id    int
 	Title string
 	Done  bool
 }
-var tasks []Task
-var taskIndex map[int]*Task
+
 func showMenu() {
 	fmt.Println("==Welcome to task manager==")
 	fmt.Println("Press 1 to add task")
@@ -42,7 +47,31 @@ tasks = append(tasks,task)
 taskIndex[id] = &tasks[len(tasks)-1]
 }
 func deleteTask(id int)bool{
-	
+if taskIndex[id] == nil {
+	return false
+}	
+index :=-1
+for i := range tasks{
+	if tasks[i].Id == id{
+		index = i
+		break
+	}
+}
+if index == -1{
+	return false
+}
+//Remove from slice
+ tasks = append(tasks[:index], tasks[index+1:]...)
+
+    // Remove from map
+    delete(taskIndex, id)
+
+    // Rebuild map pointers
+    for i := range tasks {
+        taskIndex[tasks[i].Id] = &tasks[i]
+    }
+
+    return true
 }
 // func findTask(id int)*Task{
 // 	for i := range tasks{
@@ -61,9 +90,10 @@ task := Task{
 	Id:32,
 	Done:true,
 }
-// f
-fmt.Println(task)
+
+showMenu()
 renameTask(&task,"main")
 fmt.Println(task)
+
 
 }
